95e43c054068cc1a9e57561afe2f3ecbbabe14ec
Bertrand Riviere <bertrand.riviere@gmail.com>
2023-09-18 16:59:38 +0200

    refs #30224 - minor changes - replace line by spline - replace area by areaspline - reduce pointPadding for bar - add pointPadding for stacked

--- src/components/HighchartsChart/Line.js
+++ src/components/HighchartsChart/Line.js
@@ -104,7 +104,7 @@ const Line = forwardRef(
 
               return dataPoint;
             }, s.data),
-            type: 'line',
+            type: 'spline',
             marker: {
               symbol: 'circle',
               radius: 3,
