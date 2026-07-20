b55283403c251efc39de23c0c250147c3685121d
Mathias Schaefer (molily) <molily@mailbox.org>
2026-07-20 14:39:41 +0200

    Highlight category groups

--- src/utils/barAndColumnChartRenderHandler.js
+++ src/utils/barAndColumnChartRenderHandler.js
@@ -1,8 +1,6 @@
-import { TinyColor } from '@ctrl/tinycolor';
-import * as R from 'ramda';
-
+/* global console */
 /**
- * @import { Chart, Point, PointOptionsObject, Series, SVGElement } from "highcharts"
+ * @import { Chart, Point, Series, SVGElement } from "highcharts"
  */
 
 const HIGHLIGHT_MARKER_SIZE = 5;
@@ -11,18 +9,19 @@ const HIGHLIGHT_MARKER_SIZE = 5;
  */
 const HIGHLIGHT_MARKER_GAP = 2;
 
-/**
- * Returns the padding numbers from the point shape size.
- *
- * @example
- * getPadding(point.shapeArgs.width)
- *
- * @param {number} pointWidth
- */
-const getPadding = (pointWidth) => Math.max(pointWidth * 0.1, 0.5);
+const OUTLINE_WIDTH = 1;
+const OUTLINE_GAP = 0.1;
+const OUTLINE_SUM = OUTLINE_WIDTH + OUTLINE_GAP;
 
 /**
- * Renders a background rect around highlighted points.
+ * Connects a Highcharts point object with an SVG shape without creating a strong reference to the point
+ *
+ * @type {WeakMap<Point, SVGElement>}
+ */
+const outlineRects = new WeakMap();
+
+/**
+ * Renders an outline rect around highlighted points.
  *
  * @param {Chart} chart
  * @param {Series} series
@@ -31,56 +30,67 @@ const getPadding = (pointWidth) => Math.max(pointWidth * 0.1, 0.5);
  * @param {string[]} highlightColors
  * @returns {SVGElement | undefined}
  */
-const renderHighlightBackground = (
+const renderHighlightOutline = (
   chart,
   series,
   point,
   isHighlighted,
   highlightColors,
 ) => {
-  /** @type {SVGElement | undefined} */
-  let highlightBackground = point.highlightBackground;
+  let outline = outlineRects.get(point);
 
   if (!isHighlighted) {
-    if (highlightBackground) {
-      highlightBackground.destroy();
-      point.highlightBackground = undefined;
+    if (outline) {
+      outline.destroy();
+      outlineRects.delete(point);
     }
     return;
   }
 
-  if (!(point.graphic && point.shapeArgs)) {
-    // eslint-disable-next-line no-undef
-    console.error('renderHighlightBackground: point.graphic not found');
+  const { graphic, shapeArgs } = point;
+  if (!(graphic && shapeArgs)) {
+    console.error('renderHighlightOutline: point.graphic not found');
     return;
   }
 
-  const fill = new TinyColor(highlightColors[0]).setAlpha(0.1).toRgbString();
+  // const highlightColor = new TinyColor(highlightColors[0])
+  //   .setAlpha(0.1)
+  //   .toRgbString();
+  const highlightColor = highlightColors[0];
 
-  if (!highlightBackground) {
-    highlightBackground = chart.renderer
+  if (!outline) {
+    outline = chart.renderer
       .rect()
-      .attr({ y: 0, fill })
+      .attr({ strokeWidth: 1, stroke: highlightColor, fill: 'none' })
       .css({ pointerEvents: 'none' })
       // Add rect to the parent group which already has a transformation applied, depending on the chart type (translate, rotate, flip)
-      .add(point.graphic.parentGroup);
+      .add(graphic.parentGroup);
 
-    point.highlightBackground = highlightBackground;
+    outlineRects.set(point, outline);
   }
 
-  const padding = getPadding(point.shapeArgs.width);
-
-  highlightBackground = highlightBackground.attr({
-    x: point.shapeArgs.x - padding,
-    width: point.shapeArgs.width + 2 * padding,
-    // Bar charts actually are column charts rotated by 90°,
-    // so the vertical dimension is the horizontal dimension in this case
-    height: series.type === 'bar' ? chart.plotWidth : chart.plotHeight,
+  outline = outline.attr({
+    x: shapeArgs.x - OUTLINE_SUM,
+    y: shapeArgs.y - OUTLINE_SUM,
+    width: shapeArgs.width + 2 * OUTLINE_SUM,
+    height:
+      // Bar charts are column charts rotated by 90° and mirrored,
+      // so x and y dimensions are flipped here, and y: 0 is on the right
+      series.type === 'bar'
+        ? chart.plotWidth + 2 * OUTLINE_SUM
+        : chart.plotHeight + 2 * OUTLINE_SUM,
     zIndex: -1,
   });
 
-  return highlightBackground;
+  return outline;
 };
+
+/**
+ * Connects a Highcharts point object with an SVG shape without creating a strong reference to the point
+ *
+ * @type {WeakMap<Point, SVGElement>}
+ */
+const axisMarkers = new WeakMap();
 
 /**
  * Renders a marker at the axis for highlighted points.
@@ -98,19 +108,18 @@ const renderAxisMarker = (
   isHighlighted,
   highlightColors,
 ) => {
-  /** @type {SVGElement | undefined} */
-  let axisMarker = point.highlightAxisMarker;
+  let axisMarker = axisMarkers.get(point);
 
   if (!isHighlighted) {
     if (axisMarker) {
       axisMarker.destroy();
-      point.highlightAxisMarker = undefined;
+      axisMarker.delete(point);
     }
     return;
   }
 
-  if (!(point.graphic && point.shapeArgs)) {
-    // eslint-disable-next-line no-undef
+  const { graphic, shapeArgs } = point;
+  if (!(graphic && shapeArgs)) {
     console.error('renderAxisMarker: point.graphic not found');
     return;
   }
@@ -119,41 +128,35 @@ const renderAxisMarker = (
 
   if (!axisMarker) {
     axisMarker = chart.renderer
-      .rect({
-        x: 0,
-        y: 0,
-        width: HIGHLIGHT_MARKER_SIZE,
-        height: HIGHLIGHT_MARKER_SIZE,
-        fill,
-      })
+      .rect
+      // Attributes are set below
+      ()
       .css({ pointerEvents: 'none' })
       // We cannot add the rect to the point's parent <g> since it has
       // a clip mask. The marker is positioned outside of the plot area.
       // The clip mask would cut it off.
       .add(chart.axisMarkerGroup);
 
-    point.highlightAxisMarker = axisMarker;
+    axisMarkers.set(point, axisMarker);
   }
-
-  const padding = getPadding(point.shapeArgs.width);
 
   axisMarker.attr(
     series.type === 'bar'
       ? {
-          x: point.shapeArgs.x,
-          y: chart.plotHeight - HIGHLIGHT_MARKER_SIZE,
-          width: point.shapeArgs.width,
+          // Bar charts are column charts rotated by 90° and mirrored,
+          // so x and y dimensions are flipped here, and y: 0 is on the right
+          x: shapeArgs.x,
+          y: chart.plotWidth + HIGHLIGHT_MARKER_GAP,
+          width: shapeArgs.width,
           height: HIGHLIGHT_MARKER_SIZE,
-          // fill needs to be repeated here, otherwise the fill is lost on update
           fill,
         }
       : // Column chart
         {
-          x: point.shapeArgs.x - padding,
+          x: Math.floor(shapeArgs.x - OUTLINE_SUM),
           y: chart.plotHeight + HIGHLIGHT_MARKER_GAP,
-          width: point.shapeArgs.width + 2 * padding,
+          width: Math.ceil(shapeArgs.width + 2 * OUTLINE_SUM),
           height: HIGHLIGHT_MARKER_SIZE,
-          // fill needs to be repeated here, otherwise the fill is lost on update
           fill,
         },
   );
@@ -162,16 +165,19 @@ const renderAxisMarker = (
 };
 
 /**
+ * Renders the highlight markers for the given series.
+ * Returns the active shapes
+ *
  * @param {Chart} chart
  * @param {Series} series
  * @param {string[]} highlightColors
  * @returns {SVGElement[]} activeShapes
  */
 const renderSeriesHighlight = (chart, series, highlightColors) => {
-  const seriesIsHighlighted = series.options.custom?.isHighlighted;
-
   const { renderer } = chart;
   const { points } = series;
+
+  const seriesIsHighlighted = series.options.custom?.isHighlighted;
 
   // Create container group for axis makers
   if (!chart.axisMarkerGroup) {
@@ -182,8 +188,8 @@ const renderSeriesHighlight = (chart, series, highlightColors) => {
   }
 
   // The series <g> has a transformation applied. We need to apply the same to the axis marker <g>.
-  // In a column chart, the transformation moves it to the plot area.
-  // In a bar chart, which is a column chart underneath, the transformation moves, rotates and flips it.
+  // In a vertical (column) chart, the transformation moves it to the plot area.
+  // In a horizontal (row) chart, which is a column chart underneath, the transformation moves, rotates and flips it.
   const seriesTransform = series.group.element.getAttribute('transform');
   chart.axisMarkerGroup.attr({ transform: seriesTransform });
 
@@ -195,15 +201,15 @@ const renderSeriesHighlight = (chart, series, highlightColors) => {
     const isHighlighted = seriesIsHighlighted || pointIsHighlighted;
 
     // Highlight background
-    const highlightBackground = renderHighlightBackground(
+    const highlightOutline = renderHighlightOutline(
       chart,
       series,
       point,
       isHighlighted,
       highlightColors,
     );
-    if (highlightBackground) {
-      activeShapes.push(highlightBackground);
+    if (highlightOutline) {
+      activeShapes.push(highlightOutline);
     }
 
     // Axis marker
@@ -223,38 +229,125 @@ const renderSeriesHighlight = (chart, series, highlightColors) => {
 };
 
 /**
- * Event handler called after load (initial render) and redraw (subsequent render)
+ * Draws a rect around all shapes of highlighted categories
+ *
+ * @param {Chart} chart
+ * @returns {SVGElement[]}
+ */
+const highlightCategoryGroups = (chart) => {
+  /** @type {SVGElement[]} */
+  const shapes = [];
+
+  const highlightedCategories = chart.options.custom?.highlightedCategories;
+
+  const categoryGroupIsHighlighted =
+    chart.options.custom?.categoryGroupIsHighlighted;
+
+  if (highlightedCategories && categoryGroupIsHighlighted) {
+    const highlightedCategoryCodes = highlightedCategories.map((c) => c.code);
+
+    // Gather point coordinates and group them by category
+
+    /** @typedef {{ x: number, y: number, width: number, height: number }} PointRect */
+    /** @type {Map<string, PointRect[]>} */
+    const pointRectsByCategory = new Map();
+
+    chart.series.forEach((series) => {
+      const {
+        group: { translateX, translateY },
+      } = series;
+
+      series.points.forEach((point) => {
+        const { name } = point;
+        if (!highlightedCategoryCodes.includes(name)) return;
+        let pointRects = pointRectsByCategory.get(name);
+        if (!pointRects) {
+          pointRects = [];
+          pointRectsByCategory.set(name, pointRects);
+        }
+        const rect = {
+          x: point.shapeArgs.x + translateX,
+          y: point.shapeArgs.y + translateY,
+          width: point.shapeArgs.width,
+          height: point.shapeArgs.height,
+        };
+        pointRects.push(rect);
+      });
+    });
+
+    // Draws a rectangle around the bounding box of all shapes
+    pointRectsByCategory.forEach((pointCoords) => {
+      let minX = Infinity;
+      let maxX = 0;
+      let minY = Infinity;
+      let maxY = 0;
+
+      pointCoords.forEach(({ x, y, width, height }) => {
+        const rightmost = x + width;
+        const bottommost = y + height;
+        if (x < minX) minX = x;
+        if (rightmost > maxX) maxX = rightmost;
+        if (y < minY) minY = y;
+        if (bottommost > maxY) maxY = bottommost;
+      });
+
+      const rect = chart.renderer
+        .rect()
+        .attr({
+          strokeWidth: 1,
+          stroke: 'red',
+          fill: 'rgb(255 0 0 / 0.05)',
+          x: minX,
+          width: maxX - minX,
+          y: minY,
+          height: maxY - minY,
+        })
+        .css({ pointerEvents: 'none' })
+        .add();
+
+      shapes.push(rect);
+    });
+  }
+  return shapes;
+};
+
+/**
+ * Event handler called after load (initial render) and redraw (subsequent render).
+ * Renders the highlight shapes and cleans up stale ones.
  *
  * @param {Chart} chart
  * @params {string[]} highlightColors
  */
 export const barAndColumnChartRenderHandler = (chart, highlightColors) => {
-  chart.highlightShapes ||= new Set();
+  /** @typedef {{ oecd_highlightShapes: Set<SVGElement> }} Chart */
+  chart.oecd_highlightShapes ||= new Set();
 
   // Fill the plot area for debugging
-  // chart.plotBackground.element.setAttribute('fill', 'rgb(0 255 0 / 0.05)');
+  chart.plotBackground.element.setAttribute('fill', 'rgb(0 255 0 / 0.1)');
 
-  const allSeries = chart.series;
+  /**
+   * Additional SVG elements created for highlighting
+   * @type {SVGElement[]}
+   */
+  const activeShapes = [];
 
-  if (allSeries.length === 0 && chart.highlightShapes) {
-    for (const obsoleteRect of chart.highlightShapes) {
-      obsoleteRect.destroy();
-    }
-    chart.highlightShapes.clear();
-  }
+  activeShapes.push(...highlightCategoryGroups(chart));
 
-  const activeShapes = R.compose(
-    (allShapesArray) => new Set(allShapesArray),
-    R.flatten,
-    R.map((series) => renderSeriesHighlight(chart, series, highlightColors)),
-  )(chart.series);
+  // Render highlight shapes for all active series. Aggregate the shapes in a Set.
+  const seriesHighlightShapes = chart.series
+    .map((series) => renderSeriesHighlight(chart, series, highlightColors))
+    .flat();
+  activeShapes.push(...seriesHighlightShapes);
+
+  const activeShapesSet = new Set(activeShapes);
 
   // Clean up old shapes
-  const obsoleteShapes = chart.highlightShapes.difference(activeShapes);
+  const obsoleteShapes = chart.oecd_highlightShapes.difference(activeShapesSet);
   for (const obsoleteShape of obsoleteShapes) {
     obsoleteShape.destroy();
   }
 
-  chart.highlightShapes.clear();
-  chart.highlightShapes = activeShapes;
+  // Save new shapes
+  chart.oecd_highlightShapes.clear();
+  chart.oecd_highlightShapes = activeShapesSet;
 };
