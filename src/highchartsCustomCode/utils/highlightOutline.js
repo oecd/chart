6534bba289a4688bf53845331902393dd1604ea2
Mathias Schaefer (molily) <molily@mailbox.org>
2026-08-04 12:43:31 +0200

    Tweak outline and marker size

--- src/highchartsCustomCode/utils/highlightOutline.js
+++ src/highchartsCustomCode/utils/highlightOutline.js
@@ -1,29 +1,39 @@
-export const OUTLINE_WIDTH_SMALL = 1;
-export const OUTLINE_WIDTH_MEDIUM = 1.5;
-export const OUTLINE_WIDTH_LARGE = 2;
+const OUTLINE_WIDTH_S = 0.75;
+const OUTLINE_WIDTH_M = 1;
+const OUTLINE_WIDTH_L = 1.5;
+const OUTLINE_WIDTH_XL = 2;
 
-export const OUTLINE_GAP_SMALL = 0.5;
-export const OUTLINE_GAP_MEDIUM = 0.75;
-export const OUTLINE_GAP_LARGE = 1;
+const OUTLINE_GAP_S = 0.5;
+const OUTLINE_GAP_M = 0.75;
+const OUTLINE_GAP_L = 1;
+const OUTLINE_GAP_XL = 1;
+
+const BREAKPOINT_M = 400;
+const BREAKPOINT_L = 600;
+const BREAKPOINT_XL = 800;
 
 /**
  * Returns the stroke width of outline
  * @param {number} chartWidth
  */
 export const getOutlineWidth = (chartWidth) =>
-  chartWidth >= 800
-    ? OUTLINE_WIDTH_LARGE
-    : chartWidth >= 600
-      ? OUTLINE_WIDTH_MEDIUM
-      : OUTLINE_WIDTH_SMALL;
+  chartWidth >= BREAKPOINT_XL
+    ? OUTLINE_WIDTH_XL
+    : chartWidth >= BREAKPOINT_L
+      ? OUTLINE_WIDTH_L
+      : chartWidth >= BREAKPOINT_M
+        ? OUTLINE_WIDTH_M
+        : OUTLINE_WIDTH_S;
 
 /**
  * Returns the gap between the value shape and the outline
  * @param {number} chartWidth
  */
 export const getOutlineGap = (chartWidth) =>
-  chartWidth >= 800
-    ? OUTLINE_GAP_LARGE
-    : chartWidth >= 600
-      ? OUTLINE_GAP_MEDIUM
-      : OUTLINE_GAP_SMALL;
+  chartWidth >= BREAKPOINT_XL
+    ? OUTLINE_GAP_XL
+    : chartWidth >= BREAKPOINT_L
+      ? OUTLINE_GAP_L
+      : chartWidth >= BREAKPOINT_M
+        ? OUTLINE_GAP_M
+        : OUTLINE_GAP_S;
