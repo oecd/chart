9540e8947fc1b5200f0f30d3930cb45a293ef0bf
Mathias Schaefer (molily) <molily@mailbox.org>
2026-08-26 10:44:57 +0200

    Preserve highlight order

--- src/utils/chartOptions/getBaselineAndHighlightCodes.js
+++ src/utils/chartOptions/getBaselineAndHighlightCodes.js
@@ -6,43 +6,86 @@ import { codeOrLabelEquals } from '../configUtil';
  * Returns the baseline and highlight codes,
  * determines flags used in the highlighting logic.
  *
+ * @param {{
+ * data: {
+ *   series: { label: string, code: string }[];
+ *   categories: { label: string, code: string }[];
+ * },
+ * baseline: string[];
+ * highlight: string[];
+ * }} options
  * @returns {{
  *   baselineCodes: string[];
  *   highlightCodes: string[];
  *   highlightSeriesCodes: string[];
  *   highlightCategoryCodes: string[];
- *   matchingHighlightColors: string[];
  *   isGroupedChart: boolean;
  *   isCategoryGroupHighlighted: boolean;
  * }}
  */
 export const getBaselineAndHighlightCodes = ({ data, baseline, highlight }) => {
   const entities = R.concat(data.series, data.categories);
-  const allCategoryCodes = R.map(R.prop('code'), data.categories);
-
   const baselineEntities = R.filter(
     (series) => R.any(codeOrLabelEquals(series), baseline),
     entities,
   );
   const baselineCodes = R.map(R.prop('code'), baselineEntities);
 
-  const highlightedSeries = R.filter(
-    (series) => R.any(codeOrLabelEquals(series), highlight),
-    data.series,
-  );
-  const highlightSeriesCodes = R.map(R.prop('code'), highlightedSeries);
+  // Determine series and category codes while preserving the original order
 
-  const highlightedCategories = R.filter(
-    (category) => R.any(codeOrLabelEquals(category), highlight),
-    data.categories,
+  const seriesCodes = new Set(R.map(R.prop('code'), data.series));
+  const seriesCodesByLabel = new Map(
+    R.map((s) => [s.label, s.code], data.series),
   );
-  const highlightCategoryCodes = R.map(R.prop('code'), highlightedCategories);
+  const categoryCodes = new Set(R.map(R.prop('code'), data.categories));
+  const categoryCodesByLabel = new Map(
+    R.map((s) => [s.label, s.code], data.categories),
+  );
 
-  // This is different from `highlight` which might contain codes or labels
-  const highlightCodes = R.concat(highlightSeriesCodes, highlightCategoryCodes);
+  console.log('seriesCodes', seriesCodes);
+  console.log('seriesCodesByLabel', seriesCodesByLabel);
+  console.log('categoryCodes', categoryCodes);
+  console.log('categoryCodesByLabel', categoryCodesByLabel);
+
+  // List of highlight codes.
+  // This is different from `highlight` which might contain codes *or* labels
+  /** @type {string[]} */
+  const highlightCodes = [];
+  /** @type {string[]} */
+  const highlightSeriesCodes = [];
+  /** @type {string[]} */
+  const highlightCategoryCodes = [];
+
+  highlight.forEach((codeOrLabel) => {
+    // Is it a series code?
+    if (seriesCodes.has(codeOrLabel)) {
+      highlightCodes.push(codeOrLabel);
+      highlightSeriesCodes.push(codeOrLabel);
+      return;
+    }
+    // Is it a series label?
+    const seriesCode = seriesCodesByLabel.get(codeOrLabel);
+    if (seriesCode) {
+      highlightCodes.push(seriesCode);
+      highlightSeriesCodes.push(seriesCode);
+      return;
+    }
+    // Is it a category code?
+    if (categoryCodes.has(codeOrLabel)) {
+      highlightCodes.push(codeOrLabel);
+      highlightCategoryCodes.push(codeOrLabel);
+      return;
+    } // Is it a category label?
+    const categoryCode = categoryCodesByLabel.get(codeOrLabel);
+    if (categoryCode) {
+      highlightCodes.push(categoryCode);
+      highlightCategoryCodes.push(categoryCode);
+      return;
+    }
+  });
 
   const isBaselineACategory = R.any(
-    R.includes(R.__, allCategoryCodes),
+    (baselineCode) => categoryCodes.has(baselineCode),
     baselineCodes,
   );
 
@@ -54,7 +97,8 @@ export const getBaselineAndHighlightCodes = ({ data, baseline, highlight }) => {
    * and can be highlighted as a visual group, not as individual points.
    */
   const isCategoryGroupHighlighted =
-    isGroupedChart && (isBaselineACategory || highlightedCategories.length > 0);
+    isGroupedChart &&
+    (isBaselineACategory || highlightCategoryCodes.length > 0);
 
   return {
     baselineCodes,
